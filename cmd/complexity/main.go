dcabb193d35bb944fc1faaa43ee26c9a224bf6f1
rauzh <pavel.rauzhev@optimum-is.com>
2024-11-25 22:35:20 +0300

    env exit code

--- cmd/complexity/main.go
+++ cmd/complexity/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"os"
+
 	"github.com/rauzh/go-complexity-analysis"
 	"golang.org/x/tools/go/analysis/unitchecker"
 )
 
-func main() { unitchecker.Main(complexity.Analyzer) }
+func main() {
+	os.Setenv("GOCOMPLEXITY_EXIT_CODE", "228")
+	unitchecker.Main(complexity.Analyzer)
+}
